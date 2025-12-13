ca6dc94185d410c6973a832a29e85743ff4955a2
oxxx1Storm <pluginexteragram@gmail.com>
2025-12-13 19:08:22 +1000

    update project

--- src/protocol/protocol.h
+++
@@ -1,9 +0,0 @@
-/*
-This is the source code of guprotoc for gulib v.1.0.0.
-It is licensed under GNU GPL v.2 or later.
-You should have received a copy of the license in this archive(see LICENSE).
-Copyright 2025 oxxxiStorm.
-*/
-
-#pragma once
-#include <sodium.h>
